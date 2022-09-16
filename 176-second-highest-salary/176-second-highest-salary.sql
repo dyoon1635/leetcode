6ea9614881336b4eeaa470b507cc93e27793aaa8
doy <69721596+dyoon1635@users.noreply.github.com>
2022-09-16 20:14:37 +0900

    Time: 468 ms (11.72%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,3 +1,3 @@
-select max(salary) as SecondHighestSalary
-from Employee
-where salary not in (select max(salary) from Employee)
+select (select distinct salary
+from employee
+order by salary desc limit 1 offset 1) as secondhighestsalary
